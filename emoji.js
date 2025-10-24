774936e5420a7b2047f6fe5f56a036882788b990
wwbwngp <ngp@wwbw.ai>
2025-10-24 20:26:12 +0900

    [chore] 포맷터 적용
     single -> double

--- emoji.js
+++ emoji.js
@@ -1,8 +1,8 @@
 const emojis = [
-  { icon: '😊', name: 'Smiling Face' },
-  { icon: '🚀', name: 'Rocket' },
-  { icon: '🍕', name: 'Pizza' },
-  { icon: '🐱', name: 'Cat' },
-  { icon: '🌈', name: 'Rainbow' },
-  { icon: '🎸', name: 'Guitar' }
+  { icon: "😊", name: "Smiling Face" },
+  { icon: "🚀", name: "Rocket" },
+  { icon: "🍕", name: "Pizza" },
+  { icon: "🐱", name: "Cat" },
+  { icon: "🌈", name: "Rainbow" },
+  { icon: "🎸", name: "Guitar" },
 ];
